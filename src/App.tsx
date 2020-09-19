f8fe692e8260dbb6f47300fe270d5c252a272aeb
Alex Bodea <alexandrubodeag@gmail.com>
2020-09-19 19:20:26 +0300

    Added eslint config and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import React from 'react';
+import React from 'react'
 
 function App() {
-  return (
-    <h1>The beginning..</h1>
-  );
+  return <h1>The beginning..</h1>
 }
 
-export default App;
+export default App
